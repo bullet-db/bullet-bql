9b2b0c43a534042e89c097738ede8b966600ecfc
Akshay Sarma <akshaisarma@gmail.com>
2021-06-24 11:03:01 -0700

    Adding a Lazy SerDe (#30)

---
+++ src/main/java/com/yahoo/bullet/bql/query/LazyPubSubMessageSerDe.java
@@ -0,0 +1,87 @@
+/*
+ *  Copyright 2021, Yahoo Inc.
+ *  Licensed under the terms of the Apache License, Version 2.0.
+ *  See the LICENSE file associated with the project for terms.
+ */
+package com.yahoo.bullet.bql.query;
+
+import com.yahoo.bullet.bql.BQLResult;
+import com.yahoo.bullet.bql.BulletQueryBuilder;
+import com.yahoo.bullet.common.BulletConfig;
+import com.yahoo.bullet.pubsub.Metadata;
+import com.yahoo.bullet.pubsub.PubSubMessage;
+import com.yahoo.bullet.pubsub.PubSubMessageSerDe;
+import com.yahoo.bullet.query.Query;
+
+/**
+ * This {@link PubSubMessageSerDe} is to be used to create a {@link PubSubMessage} for a query without actually creating
+ * the {@link Query} object. The {@link Query} is instead created upon use of the {@link #fromMessage(PubSubMessage)}.
+ * The {@link BulletConfig} provided to this class is used to configure the {@link BulletQueryBuilder} and the created
+ * {@link Query} object.
+ *
+ * The BQL query string is stored as the payload in the {@link PubSubMessage} and it is not set in the message
+ * {@link Metadata}. It is added back to the {@link Metadata} in {@link #fromMessage(PubSubMessage)}} and the query
+ * string is converted back to a {@link Query} using the {@link #toQuery(String)} method and configured with the
+ * provided {@link BulletConfig}.
+ *
+ * The {@link #toMessage(PubSubMessage)} does nothing by default, so you can safely use it for sending signals and other
+ * messages or even regular query {@link PubSubMessage}! Do note that this SerDe identifies its lazy Query messages by
+ * placing a {@link Metadata.Signal#CUSTOM} in its metadata. This is what it uses to process its messages in the
+ * {@link #fromMessage(PubSubMessage)}. You will want to not send other messages with that signal.
+ *
+ * Note, it is essential that the BQL query provided be valid if using the default BQL conversion in
+ * {@link #toQuery(String)}! Otherwise, {@link #fromMessage(PubSubMessage)} will throw a {@link RuntimeException} when
+ * it cannot create a {@link Query} object from the BQL query string.
+ */
+public class LazyPubSubMessageSerDe extends PubSubMessageSerDe {
+    private static final long serialVersionUID = -6866473821452218792L;
+
+    private BulletQueryBuilder queryBuilder;
+
+    /**
+     * Constructor.
+     *
+     * @param config The {@link BulletConfig} to configure this class and the {@link BulletQueryBuilder}.
+     */
+    public LazyPubSubMessageSerDe(BulletConfig config) {
+        super(config);
+        queryBuilder = new BulletQueryBuilder(config);
+    }
+
+    @Override
+    public PubSubMessage toMessage(String id, Query query, String queryString) {
+        return toMessage(new PubSubMessage(id, queryString, new Metadata(Metadata.Signal.CUSTOM, null)));
+    }
+
+    @Override
+    public PubSubMessage toMessage(PubSubMessage message) {
+        return message;
+    }
+
+    @Override
+    public PubSubMessage fromMessage(PubSubMessage message) {
+        if (!message.hasSignal(Metadata.Signal.CUSTOM)) {
+            return message;
+        }
+        String queryString = message.getContentAsString();
+        Query query = toQuery(queryString);
+        return new PubSubMessage(message.getId(), query, new Metadata(null, queryString));
+    }
+
+    /**
+     * Convert the given String query back to a {@link Query} object. Override if you stored something else instead of
+     * BQL.
+     *
+     * @param queryString The query.
+     * @return A valid {@link Query} object.
+     */
+    protected Query toQuery(String queryString) {
+        BQLResult result = queryBuilder.buildQuery(queryString);
+        if (result.hasErrors()) {
+            throw new RuntimeException("The SerDe does not handle invalid BQL!");
+        }
+        Query query = result.getQuery();
+        query.configure(config);
+        return query;
+    }
+}
---
+++ src/test/java/com/yahoo/bullet/bql/query/LazyPubSubMessageSerDeTest.java
@@ -0,0 +1,87 @@
+/*
+ *  Copyright 2021, Yahoo Inc.
+ *  Licensed under the terms of the Apache License, Version 2.0.
+ *  See the LICENSE file associated with the project for terms.
+ */
+package com.yahoo.bullet.bql.query;
+
+import com.yahoo.bullet.bql.BQLConfig;
+import com.yahoo.bullet.common.BulletConfig;
+import com.yahoo.bullet.pubsub.Metadata;
+import com.yahoo.bullet.pubsub.PubSubMessage;
+import com.yahoo.bullet.query.Projection;
+import com.yahoo.bullet.query.Query;
+import com.yahoo.bullet.query.aggregations.AggregationType;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
+public class LazyPubSubMessageSerDeTest {
+    @Test
+    public void testLazyMessage() {
+        LazyPubSubMessageSerDe serDe = new LazyPubSubMessageSerDe(new BQLConfig());
+        PubSubMessage message = serDe.toMessage("foo", null, "bar");
+        Assert.assertEquals(message.getId(), "foo");
+        Assert.assertEquals(message.getContent(), "bar");
+        Assert.assertEquals(message.getContentAsString(), "bar");
+        Metadata metadata = message.getMetadata();
+        Assert.assertEquals(metadata.getSignal(), Metadata.Signal.CUSTOM);
+        Assert.assertNull(metadata.getContent());
+        Assert.assertTrue(metadata.getCreated() <= System.currentTimeMillis());
+
+        PubSubMessage unchanged = serDe.toMessage(message);
+        Assert.assertEquals(unchanged.getId(), "foo");
+        Assert.assertEquals(unchanged.getContent(), "bar");
+        Assert.assertEquals(unchanged.getContentAsString(), "bar");
+        metadata = message.getMetadata();
+        Assert.assertEquals(metadata.getSignal(), Metadata.Signal.CUSTOM);
+        Assert.assertNull(metadata.getContent());
+        Assert.assertEquals(metadata.getCreated(), message.getMetadata().getCreated());
+    }
+
+    @Test
+    public void testIgnoringOtherMessages() {
+        LazyPubSubMessageSerDe serDe = new LazyPubSubMessageSerDe(new BQLConfig());
+        PubSubMessage result = serDe.fromMessage(new PubSubMessage("id", new HashMap<>(), (Metadata) null));
+
+        Assert.assertEquals(result.getId(), "id");
+        Assert.assertNull(result.getMetadata());
+
+        result = serDe.fromMessage(new PubSubMessage("id", new HashMap<>(), new Metadata(Metadata.Signal.KILL, null)));
+        Assert.assertEquals(result.getId(), "id");
+        Metadata metadata = result.getMetadata();
+        Assert.assertEquals(metadata.getSignal(), Metadata.Signal.KILL);
+        Assert.assertNull(metadata.getContent());
+    }
+
+    @Test
+    public void testQueryCreation() {
+        BQLConfig config = new BQLConfig();
+        config.set(BulletConfig.AGGREGATION_MAX_SIZE, 5);
+        LazyPubSubMessageSerDe serDe = new LazyPubSubMessageSerDe(config);
+
+        PubSubMessage message = serDe.toMessage("id", null, "SELECT * FROM STREAM(MAX, TIME) LIMIT 100");
+        PubSubMessage result = serDe.fromMessage(message);
+
+        Assert.assertEquals(result.getId(), "id");
+        Query actual = result.getContentAsQuery();
+        Assert.assertEquals(actual.getProjection().getType(), Projection.Type.PASS_THROUGH);
+        Assert.assertEquals(actual.getAggregation().getType(), AggregationType.RAW);
+        Assert.assertEquals((long) actual.getAggregation().getSize(), 5L);
+        Assert.assertEquals((long) actual.getDuration(), Long.MAX_VALUE);
+        Metadata metadata = result.getMetadata();
+        Assert.assertNull(metadata.getSignal());
+        Assert.assertEquals(metadata.getContent(), "SELECT * FROM STREAM(MAX, TIME) LIMIT 100");
+        Assert.assertTrue(metadata.getCreated() <= System.currentTimeMillis());
+
+        Assert.assertNotSame(message, result);
+        Assert.assertNotSame(message.getMetadata(), metadata);
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void testInvalidQueryCreation() {
+        LazyPubSubMessageSerDe serDe = new LazyPubSubMessageSerDe(new BQLConfig());
+        serDe.fromMessage(serDe.toMessage("", null, "garbage"));
+    }
+}
